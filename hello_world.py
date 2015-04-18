0ac5a8b961d6c21f1a0bc10de6a79526dbf2777f
DANCSI <dancsi@gmail.com>
2015-04-18 16:03:33 -0700

    Adding a hello from the laptop

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,7 @@ def PrintSomeSutff():
     print "Hello World"
     print "Hello from github"
     print "hello from the internet again!"
+    print "hello from the laptop"
 
 if __name__ == "__main__":
     PrintSomeSutff()
